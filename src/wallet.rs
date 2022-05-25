05ef4008b547f390942dd9d58819efd7a85b3311
M4tsuti <ctsinon@outlook.com>
2022-05-25 23:16:26 +0800

    feat. added backup for wallet

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,41 +1,100 @@
-use core::cmp::Ordering;
+use core::{cmp::Ordering, intrinsics::size_of, ptr::addr_of, slice};
 
 use chacha20::cipher::StreamCipher;
-use cortex_m::{interrupt::free};
+use cortex_m::{interrupt::free, prelude::*};
+use rand::Rng;
 
 use crate::{
     input::FIXED_KEY_LEN, 
     error::{Error, Result}, 
     update_global, 
-    global::CIPHER, set_global
+    global::{CIPHER, RNG, DELAY}, set_global
 };
 
-use self::{safe_zone::{SafeZone, EthAddr, ZKPLAIN, Signature}, utils::get_cipher};
+use self::{safe_zone::{SafeZone, EthAddr, ZKPLAIN, Signature}, utils::get_cipher, initializer::write_wallet};
 
 pub mod initializer;
 pub mod safe_zone;
 pub mod utils;
 
+pub type PubKey = [u8; 64];
+
 pub(super) const ACCOUNT_NUM: usize = 32;
 /// length of OTP secret, which is randomly generated when initializing
 pub(super) const OTP_SECRET_LEN: usize = 64;
+
+pub const WALLET_SIZE: usize = size_of::<Wallet>();
+pub const WALLET_REPEAT: usize = 128 * 1024 / size_of::<Wallet>();
 
 /// This static variable is in the flash memory, so its persistent
 /// The initial value will be changed after the wallet is initialized
 #[link_section = ".wallet"]
 #[no_mangle]
-pub static WALLET: Wallet = Wallet { 
+pub static mut WALLET: [Wallet; WALLET_REPEAT] = [Wallet { 
     initialized: false, 
     zone: SafeZone {
         zkmagic: ZKPLAIN,
         keys: [[0; 32]; ACCOUNT_NUM],
         otp_secret: [0; 64],
-    }, 
+    },
     chacha_iv: [0; 12], 
-    addrs: [[0; 20]; ACCOUNT_NUM]
-};
+    addrs: [[0; 20]; ACCOUNT_NUM],
+    pubkeys: [[0; 64]; ACCOUNT_NUM],
+    crc: 2004029866,
+}; WALLET_REPEAT];
+
+pub fn wallet() -> &'static Wallet {
+    let mut crcs = [0; WALLET_REPEAT];
+
+    unsafe {
+        let mut valid_count = 0;
+
+        for i in 0..WALLET_REPEAT {
+            let mut wallet = WALLET[i];
+            let crc = wallet.crc;
+            wallet.crc = 0;
+            let wallet_slice = slice::from_raw_parts(
+                addr_of!(wallet) as *const u8, 
+                WALLET_SIZE
+            );
+
+            if crc32fast::hash(wallet_slice) == crc {
+                crcs[i] = crc;
+                valid_count += 1;  
+            }
+            wallet.crc = crc;
+        };
+
+        if valid_count == 0 {
+            panic!("cannot recover wallet data.")
+        }
+
+        let mut counter = [0; 5];
+
+        for i in 0..5 {
+            counter[i] = crcs.iter().filter(|crc| 
+                (**crc != 0) && (**crc == crcs[i])
+            ).count();
+        }
+        
+        let (correct, count) = counter.iter().enumerate()
+            .max_by(|a, b| 
+                a.1.cmp(b.1) 
+            ).unwrap();
+        
+        if *count == valid_count {
+            return &WALLET[correct]
+        }
+
+        let wallet = WALLET[correct];
+
+        write_wallet(wallet);
+        &WALLET[0]
+    }
+}
 
 pub const WALLET_SECTOR: u8 = 8;
+pub const SECTIOR_BASE: usize = 0x0800_0000;
 
 #[repr(align(16))]
 #[derive(Clone, Copy)]
@@ -46,11 +105,34 @@ pub struct Wallet {
     /// this field is not encrypted 
     pub chacha_iv: [u8; 12],
     pub addrs: [EthAddr; ACCOUNT_NUM],
+    pub pubkeys: [PubKey; ACCOUNT_NUM],
+    pub crc: u32
 }
 
 impl Wallet {
+    pub const fn new() -> Self {
+        Self { 
+            initialized: false, 
+            zone: SafeZone {
+                zkmagic: ZKPLAIN,
+                keys: [[0; 32]; ACCOUNT_NUM],
+                otp_secret: [0; 64],
+            },
+            chacha_iv: [0; 12], 
+            addrs: [[0; 20]; ACCOUNT_NUM],
+            pubkeys: [[0; 64]; ACCOUNT_NUM],
+            crc: 0,
+        }
+    }
+
     pub fn sign_raw(&self, idx: usize, raw: &[u8]) -> Result<Signature> {
-        update_global!(|mut cipher: Option<CIPHER>| {
+        update_global!(|
+            mut cipher: Option<CIPHER>, 
+            mut rng: Option<RNG>,
+            mut delay: Option<DELAY>
+        | {
+            let delay_time: u32 = rng.gen();
+            delay.delay_us(delay_time % 10000);
             self.zone.sign_raw(idx, raw, &mut cipher)
         })
     }
--- src/wallet/initializer.rs
+++ src/wallet/initializer.rs
@@ -1,27 +1,28 @@
-use core::{ptr::addr_of, intrinsics::size_of, slice};
+use core::{ptr::addr_of, intrinsics::size_of, slice, iter::repeat};
 
 use chacha20::{
     ChaCha20,
     cipher::{
         StreamCipher, 
-        generic_array::sequence::Split
+        generic_array::{sequence::Split, GenericArray}, consts::U12
     }
 };
 
+use cortex_m::prelude::_embedded_hal_blocking_delay_DelayUs;
 use k256::{self, ecdsa::SigningKey, elliptic_curve::sec1::ToEncodedPoint};
 use rand::Rng;
 use sha3::{Keccak256, Digest};
 use stm32f4xx_hal::flash::FlashExt;
 
-use crate::{global::*, update_global, input::KeyInputBuffer, wallet::WALLET_SECTOR};
+use crate::{global::*, update_global, input::KeyInputBuffer, wallet::{WALLET_SECTOR, SECTIOR_BASE, WALLET_REPEAT}};
 
-use super::{ACCOUNT_NUM, utils::get_cipher};
+use super::{ACCOUNT_NUM, utils::get_cipher, wallet};
 use super::{Wallet, WALLET};
 use crate::error::Result;
 
 /// check if the wallet is initialized. If not, initialize it.
 pub fn try_initialize_wallet() -> Result<()> {
-    if WALLET.initialized {
+    if !wallet().initialized {
         // TODO get a user input password from keyboard
         let passcode = KeyInputBuffer::wait_for_key();
         initialize_wallet(passcode);
@@ -30,12 +31,39 @@ pub fn try_initialize_wallet() -> Result<()> {
     Ok(())
 }
 
+pub fn write_wallet(mut wallet: Wallet) {
+    wallet.crc = 0;
+
+    let stack_wallet_addr = addr_of!(wallet);
+    let flash_wallet_addr = unsafe { addr_of!(WALLET) };
+    let wallet_size = size_of::<Wallet>();
+
+    // program the wallet to flash
+    update_global!(|mut flash: Option<FLASH>, mut led: Option<LED>| {
+        let wallet_slice = unsafe {
+            slice::from_raw_parts(stack_wallet_addr as *const u8, wallet_size)
+        };
+        wallet.crc = crc32fast::hash(wallet_slice);
+
+        let mut unlocked = flash.unlocked();
+        unlocked.erase(WALLET_SECTOR).unwrap();
+        unlocked.program(
+            flash_wallet_addr as usize - SECTIOR_BASE, 
+            repeat(wallet_slice.iter()).take(WALLET_REPEAT).flatten()
+        ).unwrap();
+
+        led.set_high();
+    })
+}
+
 fn initialize_wallet(passcode: [u8; 8]) {
-    let iv: [u8; 12] = update_global!(|mut rng: Option<RNG>| {
+    let iv: [u8; 12] = update_global!(|mut rng: Option<RNG>, mut delay: Option<DELAY>| {
+        let delay_time: u32 = rng.gen();
+        delay.delay_us(delay_time % 4300);
         rng.gen()
     });
 
-    let mut wallet = WALLET;
+    let mut wallet = Wallet::new();
     
     wallet.chacha_iv = iv;
 
@@ -46,22 +74,7 @@ fn initialize_wallet(passcode: [u8; 8]) {
     // initialize OTP
     wallet.initialized = true;
 
-    let wallet_addr = addr_of!(wallet);
-    let wallet_size = size_of::<Wallet>();
-
-    // program the wallet to flash
-    update_global!(|mut flash: Option<FLASH>| {
-        let wallet_slice = unsafe {
-            slice::from_raw_parts(wallet_addr as *const u8, wallet_size)
-        };
-
-        let mut unlocked = flash.unlocked();
-        unlocked.erase(WALLET_SECTOR).unwrap();
-        unlocked.program(
-            wallet_addr as usize, 
-            wallet_slice.iter()
-        ).unwrap();
-    })
+    write_wallet(wallet);
 }
 
 /// generate accounts
